3954ece07b5b3315c8b3233601da190195ed2d48
Chen Dar-Yen <chendaryen@outlook.com>
2021-10-26 19:39:08 +0100

    test

--- taming/modules/losses/vqperceptual.py
+++ taming/modules/losses/vqperceptual.py
@@ -80,8 +80,8 @@ class VQLPIPSWithDiscriminator(nn.Module):
             nll_grads = torch.autograd.grad(nll_loss, self.last_layer[0], retain_graph=True)[0]
             g_grads = torch.autograd.grad(g_loss, self.last_layer[0], retain_graph=True)[0]
 
-        d_weight = torch.norm(nll_grads) / (torch.norm(g_grads) + 1e-4)
-        d_weight = torch.clamp(d_weight, 0.0, 1e4).detach()
+        d_weight = torch.norm(g_grads) / (torch.norm(nll_grads) + 1e-4)
+        d_weight = torch.clamp(d_weight, 0.1, 1e4).detach()
         return d_weight
 
     def forward(self, codebook_loss, latent_var, latent_mean, inputs, reconstructions, fake, optimizer_idx,
