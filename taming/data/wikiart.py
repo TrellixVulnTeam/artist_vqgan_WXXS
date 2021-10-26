92af67ee418c4cd2f0e62c7f8710cd57b3894375
Chen Dar-Yen <chendaryen@outlook.com>
2021-10-26 13:19:25 +0100

    Revert "rwst"
    This reverts commit e08e90d381dd40cc00378407e94f2b54ea61ac59.

--- taming/data/wikiart.py
+++ taming/data/wikiart.py
@@ -80,7 +80,7 @@ class WikiArtTrain(WikiArtBase):
         print(f'total {len(self.data)} training data.')
 
     def __len__(self):
-        return 120
+        return 6
 
 
 class WikiArtValidation(WikiArtBase):
